e305ab82b9b1f3d5c144a15a87a49c6245035068
alone <1584060557@qq.com>
2020-06-05 08:29:04 +0800

    内部类

---
+++ src/main/java/innerclass/ClassInInterface.java
@@ -0,0 +1,20 @@
+package innerclass;
+
+/**
+ * @author: 李昭
+ * @Date: 2020/6/5 7:28
+ */
+public interface ClassInInterface {
+    void howdy();
+    class Test implements ClassInInterface {
+
+        @Override
+        public void howdy() {
+            System.out.println("Howdy!");
+        }
+
+        public static void main(String[] args) {
+            new Test().howdy();
+        }
+    }
+}
